e34967a6d27adcfa1a8d35effe190a522708f240
Amar Tubic <73004912+EvillDeadSpace@users.noreply.github.com>
2023-09-19 02:09:46 +0200

    Update v2

---
+++ frontend/src/components/scrap/ServicesList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ServicesCard from './ServicesCard';
+import services from './services';
+
+const ServicesList = () => {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+            {services.map((item, index) => (
+                <ServicesCard item={item} index={index} key={index} />
+            ))}
+        </div>
+    );
+};
+
+export default ServicesList;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import PlaningSlika from "../assets/picture/plalingSlika.jpg"
 import Phone from "../assets/picture/Power Moves - Calendar.png"
 import About from './About'
 import { BsArrowRight } from 'react-icons/bs'
+import ServicesList from '../components/scrap/ServicesList'
 const Home = () => {
   return (
     <>
@@ -76,8 +77,17 @@ const Home = () => {
           </div>
         </div>
       </section>
-
       <About />
+      {/*   services */}
+      <section className=''>
+        <div className="container">
+          <div className='xl:w-[470px]  mx-auto'>
+            <h2 className='heading  text-center mt-20'>Empowering Your Fitness Journey Online</h2>
+            <p className=' text_paragraph text-center'>Join our virtual fitness community and transform your workouts. Discover personalized training, connect with expert trainers, and book with ease. Reimagine fitness, online.</p>
+          </div>
+          <ServicesList />
+        </div>
+      </section >
 
 
     </>
