79d7fa30ead75b0190634839430703ea3beb0e95
Amar Tubic <73004912+EvillDeadSpace@users.noreply.github.com>
2023-10-16 03:21:43 +0200

    Test Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react';
-import Layout from './layout/Layout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import './global.css';
-import Home from './pages/Home'
+import { useState } from "react";
+import Layout from "./layout/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./Global.css";
+import Home from "./pages/Home";
 
-import Wraper from './Wraper';
-import LoadingBar from 'react-top-loading-bar'
-
+import Wraper from "./Wraper";
+import LoadingBar from "react-top-loading-bar";
 
 function App() {
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0);
 
   return (
     <>
-      <Wraper >
-        <LoadingBar color='#f11946' progress={progress} onLoaderFinished={() => setProgress(0)} />
+      <Wraper>
+        <LoadingBar
+          color="#f11946"
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
         <Layout setProgress={setProgress} />
       </Wraper>
     </>
